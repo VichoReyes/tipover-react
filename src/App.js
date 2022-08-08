5f5bba3763156ac164c5e4ecf548be889edf1fe1
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-08-07 22:31:36 -0400

    allow dropping of crates on board

--- src/App.js
+++ src/App.js
@@ -1,18 +1,144 @@
 import 'pattern.css/pattern.scss'
 import './App.css';
+import {colorLengths, darkColors, directionUnitVector} from "./constants";
+import {useState} from "react";
 
-const firstCard = {
+/*
+There are three different board representations, depending on
+where they are used.
+
+1. Card:
+
+This is a representation of the initial board state, without having made any moves.
+The priority is ease of writing:
+
+{
+  blue: [],
   green: [[3, 0], [0, 1]],
   yellow: [[4, 2]],
   red: [[1, 4]],
   start: [4, 2],
 }
 
-const darkColors = {
-  green: "darkgreen",
-  red: "darkred",
-  yellow: "darkorange",
-  blue: "darkblue",
+blue, green, yellow and red are lists of coordinates of the initial places of crates.
+start is the coordinates of the initial location of the player.
+
+
+2. Card + Moves + Player
+
+This is the representation used in the Board's useState.
+
+{
+  card: (insert card representation here),
+  moves: [
+    {
+      box: [4, 2],
+      direction: "up",
+    }, {
+      box: [3, 0],
+      direction: "left",
+    }
+  ],
+  player: [0, 0] // current location
+}
+
+
+3. Generated table
+
+From representation 2, the function fillBoard
+generates a grid of squares. Each square is like
+
+{
+  color: "blue",
+  coords: [3, 0],
+  type: "standing" // or "fall-base" or "fall-top"
+}
+
+From this representation, the Board component is made.
+ */
+
+function fillBoard(card, moves, width, height) {
+  let board = [];
+  for (let i = 0; i < width; i++) {
+    let row = [];
+    for (let j = 0; j < height; j++) {
+      row.push({
+        color: null,
+        key: [j, i],
+      });
+    }
+    board.push(row);
+  }
+
+  function getInBoard(x, y) {
+    if (board.length > y && board[y].length > x) {
+      return board[y][x];
+    }
+    return null;
+  }
+
+  function setInBoard(x, y, contents) {
+    console.assert(board.length > y && board[y].length > x);
+    board[y][x] = contents
+  }
+
+  function initializeAllOf(crateColor) {
+    for (const coords of card[crateColor] || []) {
+      if (!coords) {
+        return
+      }
+      const [xCoord, yCoord] = coords
+      setInBoard(xCoord, yCoord, {
+        color: crateColor,
+        key: coords,
+        type: "standing",
+      });
+    }
+  }
+
+  for (const crateColor of ["blue", "green", "yellow", "red"]) {
+    initializeAllOf(crateColor);
+  }
+
+  function moveCrate(coords, direction) {
+    const [crateX, crateY] = coords;
+    const crateInBoard = getInBoard(crateX, crateY);
+
+    // we can only drop a crate if it's standing
+    console.assert(crateInBoard.type === "standing")
+
+    const [directionX, directionY] = directionUnitVector[direction];
+
+    // check fallen locations for emptiness
+    for (let i = 1; i <= colorLengths[crateInBoard.color]; i++) {
+      const [destX, destY] = [crateX + i * directionX, crateY + i * directionY]
+      const destInBoard = getInBoard(destX, destY);
+      // make sure dest place exists and doesn't have contents
+      console.assert(destInBoard && !destInBoard.color);
+    }
+
+    // checks passed, let's drop the crate
+    for (let i = 1; i <= colorLengths[crateInBoard.color]; i++) {
+      const [destX, destY] = [crateX + i * directionX, crateY + i * directionY]
+      const destInBoard = getInBoard(destX, destY);
+      destInBoard.color = crateInBoard.color
+      destInBoard.type = (i === 1 ? "fall-base" : "fall-top");
+    }
+    crateInBoard.color = null;
+    crateInBoard.type = null;
+  }
+
+  for (const {box, direction} of moves) {
+    moveCrate(box, direction);
+  }
+  return board;
+}
+
+const firstCard = {
+  green: [[3, 0], [0, 1]],
+  yellow: [[4, 2]],
+  red: [[1, 4]],
+  start: [4, 2],
 }
 
 function GenericCell({color, type}) {
@@ -33,42 +159,12 @@ function GenericCell({color, type}) {
   return <td style={style} className={className}></td>;
 }
 
-function genBoard(width, height) {
-  let board = [];
-  for (let i = 0; i < width; i++) {
-    let row = [];
-    for (let j = 0; j < height; j++) {
-      row.push({
-        color: null,
-        key: [j, i],
-      });
-    }
-    board.push(row);
-  }
-  const card = firstCard;
-
-  function initializeAllOf(crateColor) {
-    for (const coords of card[crateColor] || []) {
-      if (!coords) {
-        return
-      }
-      const [xCoord, yCoord] = coords
-      board[yCoord][xCoord] = {
-        color: crateColor,
-        key: coords,
-        type: "standing",
-      }
-    }
-  }
-
-  for (const crateColor of ["blue", "green", "yellow", "red"]) {
-    initializeAllOf(crateColor);
-  }
-  return board;
-}
-
 function App() {
-  const board = genBoard(6, 6);
+  const [gameState] = useState({
+    card: firstCard,
+    moves: [{box: [4, 2], direction: "up"}, {box: [3, 0], direction: "left"}],
+  })
+  const board = fillBoard(gameState.card, gameState.moves, 6, 6)
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const directionUnitVector = {
+  up: [0, -1],
+  down: [0, 1],
+  left: [-1, 0],
+  right: [1, 0]
+};
+export const darkColors = {
+  green: "darkgreen",
+  red: "darkred",
+  yellow: "darkorange",
+  blue: "darkblue",
+}
+export const colorLengths = {
+  red: 1,
+  yellow: 2,
+  green: 3,
+  blue: 4,
+}
