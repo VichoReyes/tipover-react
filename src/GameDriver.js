9e14d4fb980c4b571252fc5982c86acd304b3c34
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-27 12:09:42 -0300

    improve Congrats visually

--- src/GameDriver.js
+++ src/GameDriver.js
@@ -8,16 +8,19 @@ function Congrats() {
     <p className="text-3xl p-6">
       Congrats! You won!
     </p>
-    <Link to="/levels">Back to levels</Link>
+    <Link className="mx-auto block w-fit border rounded p-1" to="/levels">Back to levels</Link>
   </>;
 }
 
 export function GameDriver() {
   const {level_id} = useParams();
-  const card = cards[Number.parseInt(level_id)];
   const [finished, setFinished] = useState(false);
+  const card = cards[Number.parseInt(level_id)];
+  if (!card) {
+    return <p>404 error (TODO: make it a real status code error)</p>
+  }
   return (
-    <main className="flex flex-row content-center justify-center">
+    <main className="mx-auto w-fit">
       {finished
         ? <Congrats/>
         : <Board finish={() => setFinished(true)} card={card}/>
