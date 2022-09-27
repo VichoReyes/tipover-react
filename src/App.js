e03844d14dc3e4ce600a1523ff60f934f3cf0e8b
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-27 10:38:48 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Board from "./Board";
 import {useState} from "react";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 
 function Congrats() {
   return <p className="text-3xl p-6">
@@ -8,10 +9,37 @@ function Congrats() {
 }
 
 export function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav className="relative w-full bg-gray-300 dark:bg-gray-900">
+          <ul className="flex flex-row pl-3">
+            <li className="p-4">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="p-4">
+              <Link to="/users">Users</Link>
+            </li>
+            <li className="p-4">
+              <a href="https://github.com/VichoReyes/tipover-react/">Source on GitHub</a>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/">
+            <GameDriver />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export function GameDriver() {
   const [finished, setFinished] = useState(false);
   return (
-    <div className="dark:text-gray-200 dark:bg-gray-800 text-gray-800 bg-gray-100">
-      <main className="min-h-screen flex flex-row content-center justify-center">
+    <div>
+      <main className="flex flex-row content-center justify-center">
         {finished
           ? <Congrats/>
           : <Board finish={() => setFinished(true)}/>
