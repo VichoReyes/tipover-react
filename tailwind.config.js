aafaa11a1bb510e2a313afc7b02675d663113818
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-30 13:39:14 -0300

    fix tailwind config mistake

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,6 +8,7 @@ module.exports = {
   theme: {
     screens: {
       'xs': '350px',
+      ...defaultTheme.screens,
     },
     extend: {},
   },
